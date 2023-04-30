daa1f5dcd1c3c27889e9ed9d6eb56339fef9115d
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-04-30 12:25:13 +0300

    creates message for validate

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -37,7 +37,7 @@ const deleteById = async (req, res) => {
   }
 
   res.json({
-    message: "Delete success",
+    message: "contact deleted",
   });
   // res.status(204).send();
 };
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,20 @@
 const Joi = require("joi");
 
 const addSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().min(3).max(30).required().messages({
+    "string.min":
+      "{{#label}} length must be at least {{#limit}} characters long",
+    "string.max":
+      "{{#label}} length must be less than or equal to {{#limit}} characters long",
+    "any.required": "missing required {{#label}} field",
+  }),
+  email: Joi.string().email().required().messages({
+    "string.email": "{{#label}} must be a valid email",
+    "any.required": "missing required {{#label}} field",
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": "missing required {{#label}} field",
+  }),
 });
 
 module.exports = {
