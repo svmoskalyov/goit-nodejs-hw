c521b0b122c3b44fbb37354b03e9297c3682529e
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-05-16 13:34:56 +0300

    creates app

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,14 @@ const { Contact } = require("../models/contact");
 const { ctrlWrapper, HttpError } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
   res.json(result);
 };
 
@@ -17,7 +24,8 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
@@ -32,9 +40,7 @@ const updateById = async (req, res) => {
 
 const updateStatusContact = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
   }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,19 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
-router.get("/", ctrl.getAll);
-router.get("/:id", isValidId, ctrl.getById);
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
-router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.get("/", authenticate, ctrl.getAll);
+router.get("/:id", authenticate, isValidId, ctrl.getById);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
+router.put(
+  "/:id",
+  authenticate,
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateById
+);
 router.patch(
   "/:id/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateStatusContactSchema),
   ctrl.updateStatusContact
 );
-router.delete("/:id", isValidId, ctrl.deleteById);
+router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
 
 module.exports = router;
