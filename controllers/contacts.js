afd9892d2422ba460927dfdbd74bbe1d1a524d0a
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-05-17 15:37:28 +0300

    query favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,13 +4,18 @@ const { ctrlWrapper, HttpError } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "name email");
+  const result = await Contact.find(
+    favorite ? { owner, favorite } : { owner },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "name email");
+
   res.json(result);
 };
 
